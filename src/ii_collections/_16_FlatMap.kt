db1770fac18ad74fbaac76f90ac1fbbdd83c8db6
guozhong <guozhong@qeeniao.com>
2017-01-17 17:36:30 +0800

    complite 17

--- src/ii_collections/_16_FlatMap.kt
+++ src/ii_collections/_16_FlatMap.kt
@@ -9,10 +9,12 @@ fun example() {
 
 val Customer.orderedProducts: Set<Product> get() {
     // Return all products ordered by customer
-    todoCollectionTask()
+//    todoCollectionTask()
+    return orders.flatMap { it.products.toList() }.toSet()
 }
 
 val Shop.allOrderedProducts: Set<Product> get() {
     // Return all products that were ordered by at least one customer
-    todoCollectionTask()
+//    todoCollectionTask()
+    return customers.flatMap { it.orderedProducts.toList() }.toSet()
 }
