4134a018fa1584978cf0bd0687506daca6164948
guozhong <guozhong@qeeniao.com>
2017-02-13 14:35:58 +0800

    complete 25

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,16 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.util.*
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        val calendar1 = Calendar.getInstance();
+        val calendar2 = Calendar.getInstance();
+        calendar1.set(this.year, this.month, this.dayOfMonth)
+        calendar2.set(other.year, other.month, other.dayOfMonth)
+        return (calendar1.timeInMillis - calendar2.timeInMillis).toInt()
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
