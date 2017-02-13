83713a81762ffedfbde4fe10d7f389c7f9addab8
guozhong <guozhong@qeeniao.com>
2017-02-13 19:47:34 +0800

    complete 27

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -8,11 +8,13 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
         val calendar2 = Calendar.getInstance();
         calendar1.set(this.year, this.month, this.dayOfMonth)
         calendar2.set(other.year, other.month, other.dayOfMonth)
-        return ((calendar1.timeInMillis - calendar2.timeInMillis)/1000).toInt()
+        return ((calendar1.timeInMillis - calendar2.timeInMillis) / 1000).toInt()
     }
 }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange {
+    return DateRange(this, other)
+}
 
 enum class TimeInterval {
     DAY,
